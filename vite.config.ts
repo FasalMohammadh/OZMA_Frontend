ccb3d94dc5f0817c3c7f4d14f92d8d0ba0e4bb10
fasalmohammadh <fazalmohammed9367@gmail.com>
2023-02-25 22:45:41 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-import * as path from 'path';
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/dist",
   plugins: [react()],
   resolve: {
-    alias: { '@': path.resolve(__dirname + '/src') },
+    alias: { "@": path.resolve(__dirname + "/src") },
   },
 });
